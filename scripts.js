9908e33d4d58aef6808aff06e0f7d6c60a86eb1b
lgnv <43182296+lgnv@users.noreply.github.com>
2020-09-23 16:18:37 +0500

    done

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const blocks = document.querySelectorAll('.info-block');
+window.addEventListener('scroll', _ => {
+    for (const block of blocks) {
+        const a = getCoords(block)
+        if (pageYOffset + window.innerHeight >= a.top) {
+            console.log(block.classList);
+            if (!block.classList.contains('show-block')) {
+                block.classList.remove('hidden');
+                block.classList.add('show-block');
+            }
+        }
+    }
+});
+
+function getCoords(elem) {
+    let box = elem.getBoundingClientRect();
+  
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
+  }
